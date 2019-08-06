a4cbab6550eeb9973f43c909f0c16d2b012a5f72
Ania Kubow <kubowania@gmail.com>
2019-08-06 16:03:00 +0100

    refactoring code

--- js/app.js
+++ js/app.js
@@ -7,14 +7,18 @@ document.addEventListener('DOMContentLoaded', () => {
   const span = document.getElementsByClassName('close')[0]
   const scoreDisplay = document.querySelector('.score-display')
   const linesDisplay = document.querySelector('.lines-score')
+  const displaySquares = document.querySelectorAll('.previous-grid div')
+  const displayWidth = 4
+  const displayIndex = 0
   let currentIndex = 0
   let currentRotation = 0
+  let currentPosition = 4
   const width = 10
   let score = 0
   let lines = 0
   let timerId
   let nextRandom = 0
-  const colors = [
+  const backgroundImages = [
     'url(images/blue_block.png)',
     'url(images/pink_block.png)',
     'url(images/purple_block.png)',
@@ -24,11 +28,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //assign functions to keycodes
   function control(e) {
+    // TODO: refactor to switch
     if(e.keyCode === 39) {
       moveright()
-    } else if (e.keyCode ===38) {
+    } else if (e.keyCode === 38) {
       rotate()
-    } else if (e.keyCode ===37) {
+    } else if (e.keyCode === 37) {
       moveleft()
     } else if (e.keyCode === 40) {
       moveDown()
@@ -38,38 +43,38 @@ document.addEventListener('DOMContentLoaded', () => {
 
   //The Tetrominoes
   const lTetromino = [
-    [1,width+1,width*2+1,2],
-    [width,width+1,width+2,width*2+2],
-    [1,width+1,width*2+1,width*2],
-    [width,width*2,width*2+1,width*2+2]
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
   ]
 
   const zTetromino = [
-    [0,width,width+1,width*2+1],
-    [width+1, width+2,width*2,width*2+1],
-    [0,width,width+1,width*2+1],
-    [width+1, width+2,width*2,width*2+1]
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1],
+    [0, width, width+1, width*2+1],
+    [width+1, width+2, width*2, width*2+1]
   ]
 
   const tTetromino = [
-    [1,width,width+1,width+2],
-    [1,width+1,width+2,width*2+1],
-    [width,width+1,width+2,width*2+1],
-    [1,width,width+1,width*2+1]
+    [1, width, width+1, width+2],
+    [1, width+1, width+2, width*2+1],
+    [width, width+1, width+2, width*2+1],
+    [1, width, width+1, width*2+1]
   ]
 
   const oTetromino = [
-    [0,1,width,width+1],
-    [0,1,width,width+1],
-    [0,1,width,width+1],
-    [0,1,width,width+1]
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1]
   ]
 
   const iTetromino = [
-    [1,width+1,width*2+1,width*3+1],
-    [width,width+1,width+2,width+3],
-    [1,width+1,width*2+1,width*3+1],
-    [width,width+1,width+2,width+3]
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
   ]
 
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
@@ -78,15 +83,11 @@ document.addEventListener('DOMContentLoaded', () => {
   let random = Math.floor(Math.random()*theTetrominoes.length)
   let current = theTetrominoes[random][currentRotation]
 
-
-  //move the Tetromino moveDown
-  let currentPosition = 4
-
   //draw the shape
   function draw() {
     current.forEach( index => {
       squares[currentPosition + index].classList.add('block')
-      squares[currentPosition + index].style.backgroundImage = colors[random]
+      squares[currentPosition + index].style.backgroundImage = backgroundImages[random]
     })
   }
 
@@ -180,16 +181,12 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   //show previous tetromino in scoreDisplay
-  const displayWidth = 4
-  const displaySquares = document.querySelectorAll('.previous-grid div')
-  let displayIndex = 0
-
   const smallTetrominoes = [
-    [1,displayWidth+1,displayWidth*2+1,2], /* lTetromino */
-    [0,displayWidth,displayWidth+1,displayWidth*2+1],  /* zTetromino */
-    [1,displayWidth,displayWidth+1,displayWidth+2],    /* tTetromino */
-    [0,1,displayWidth,displayWidth+1],     /* oTetromino */
-    [1,displayWidth+1,displayWidth*2+1,displayWidth*3+1]  /* iTetromino */
+    [1, displayWidth+1, displayWidth*2+1, 2], /* lTetromino */
+    [0, displayWidth, displayWidth+1, displayWidth*2+1],  /* zTetromino */
+    [1, displayWidth, displayWidth+1, displayWidth+2],    /* tTetromino */
+    [0, 1, displayWidth, displayWidth+1],     /* oTetromino */
+    [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1]  /* iTetromino */
   ]
 
   function displayShape() {
@@ -199,14 +196,17 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     smallTetrominoes[nextRandom].forEach( index => {
       displaySquares[displayIndex + index].classList.add('block')
-      displaySquares[displayIndex + index].style.backgroundImage = colors[nextRandom]
+      displaySquares[displayIndex + index].style.backgroundImage = backgroundImages[nextRandom]
     })
   }
 
   //Add score
   function addScore() {
     for (currentIndex = 0; currentIndex < 199;currentIndex += width) {
-      const row = [currentIndex,currentIndex+1,currentIndex+2,currentIndex+3,currentIndex+4,currentIndex+5,currentIndex+6,currentIndex+7,currentIndex+8,currentIndex+9]
+
+      const row = []
+      for(let i = 0; i<10; i++) row.push(currentIndex + i)
+
       if(row.every(index => squares[index].classList.contains('block2'))) {
         score += 10
         lines +=1
